97de1a0cef3501a7a12fbb6bcc1179be465f06eb
SwagDispenser <dimadovgesh@gmail.com>
2025-05-12 08:29:59 +0300

    Node structure

---
+++ Node.h
@@ -0,0 +1,18 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include <memory>
+
+template <typename T>
+class Node {
+public:
+    T data;
+    std::shared_ptr<Node<T>> next;
+    std::weak_ptr<Node<T>> prev;
+
+    Node(const T& value) : data(value), next(nullptr), prev() {}
+
+    ~Node() = default;
+};
+
+#endif // NODE_H
